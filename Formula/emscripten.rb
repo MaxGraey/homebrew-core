d642f2a8514546761ab2d09ebd08a4d2d473f9b7
MaxGraey <maxgraey@gmail.com>
2020-07-05 15:05:10 +0300

    init

--- Formula/emscripten.rb
+++ Formula/emscripten.rb
@@ -7,16 +7,6 @@ class Emscripten < Formula
   stable do
     url "https://github.com/emscripten-core/emscripten/archive/1.39.18.tar.gz"
     sha256 "aa0df2828096d161636467d4fc062bbeea31e372c122a05c9685bb6cb2d4064e"
-
-    resource "fastcomp" do
-      url "https://github.com/emscripten-core/emscripten-fastcomp/archive/1.39.18.tar.gz"
-      sha256 "5f517b4347217073dcdbb63ba94577fbf75260c30f597a05120a2cf542d66cdf"
-    end
-
-    resource "fastcomp-clang" do
-      url "https://github.com/emscripten-core/emscripten-fastcomp-clang/archive/1.39.18.tar.gz"
-      sha256 "f17150527cd6130cce609a276b30a402e647211c7b71d6a48344b537215ec9d0"
-    end
   end
 
   bottle do
@@ -27,38 +17,27 @@ class Emscripten < Formula
   end
 
   head do
-    url "https://github.com/emscripten-core/emscripten.git", :branch => "incoming"
-
-    resource "fastcomp" do
-      url "https://github.com/emscripten-core/emscripten-fastcomp.git", :branch => "incoming"
-    end
-
-    resource "fastcomp-clang" do
-      url "https://github.com/emscripten-core/emscripten-fastcomp-clang.git", :branch => "incoming"
-    end
+    url "https://github.com/emscripten-core/emscripten.git", :branch => "master"
   end
 
   depends_on "cmake" => :build
+  depends_on "llvm"
   depends_on "binaryen"
   depends_on "node"
   depends_on "python@3.8"
   depends_on "yuicompressor"
 
   def install
-    ENV.cxx11
+    ENV.cxx14
 
     # All files from the repository are required as emscripten is a collection
     # of scripts which need to be installed in the same layout as in the Git
     # repository.
     libexec.install Dir["*"]
 
-    (buildpath/"fastcomp").install resource("fastcomp")
-    (buildpath/"fastcomp/tools/clang").install resource("fastcomp-clang")
-
-    cmake_args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"] }
     cmake_args = [
       "-DCMAKE_BUILD_TYPE=Release",
-      "-DCMAKE_INSTALL_PREFIX=#{libexec}/llvm",
+      "-DLLVM_ENABLE_PROJECTS='lld;clang'",
       "-DLLVM_TARGETS_TO_BUILD='X86;JSBackend'",
       "-DLLVM_INCLUDE_EXAMPLES=OFF",
       "-DLLVM_INCLUDE_TESTS=OFF",
@@ -67,7 +46,7 @@ class Emscripten < Formula
       "-DGO_EXECUTABLE=/usr/bin/false",
     ]
 
-    mkdir "fastcomp/build" do
+    mkdir "upstream/build" do
       system "cmake", "..", *cmake_args
       system "make"
       system "make", "install"
@@ -82,16 +61,6 @@ class Emscripten < Formula
        emranlib emrun emscons].each do |emscript|
       (bin/emscript).write_env_script libexec/emscript, :PYTHON => Formula["python@3.8"].opt_bin/"python3"
     end
-  end
-
-  def caveats
-    <<~EOS
-      Manually set LLVM_ROOT to
-        #{opt_libexec}/llvm/bin
-      and BINARYEN_ROOT to
-        #{Formula["binaryen"].opt_prefix}
-      in ~/.emscripten after running `emcc` for the first time.
-    EOS
   end
 
   test do
